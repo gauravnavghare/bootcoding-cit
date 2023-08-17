8badf0a8d83af423e5fa075198962717dea0d691
gauravnavghare <gn2618pbn1@gmail.com>
2023-08-17 15:52:34 +0530

    java programs

---
+++ src/java/hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String[] args) {
+        int hours = 10;
+        int minutes =30;
+        int second = minutes*60;
+        int hourSeconds = hours * 60 * 60;
+        System.out.println("hour to seconds: "+hourSeconds);
+        System.out.println("Minutes to seconds:"+ second);
+    }
+}
